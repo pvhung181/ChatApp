15cb067310484b18443304569cc985b237fad3db
pvhung <hungdev27102003@gmail.com>
2024-04-04 22:36:49 +0700

    refactor: remove nav graph, add navigation bottom app bar

--- app/src/main/java/vn/pvhung/appchat/activities/home/HomeActivity.java
+++ app/src/main/java/vn/pvhung/appchat/activities/home/HomeActivity.java
@@ -1,17 +1,25 @@
 package vn.pvhung.appchat.activities.home;
 
+import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
-
-import android.content.Intent;
-import android.os.Bundle;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-import vn.pvhung.appchat.MainActivity;
+import java.util.List;
+
+import vn.pvhung.appchat.R;
 import vn.pvhung.appchat.databinding.ActivityHomeBinding;
+import vn.pvhung.appchat.fragments.friends.FriendFragment;
+import vn.pvhung.appchat.fragments.home.HomeFragment;
+import vn.pvhung.appchat.fragments.setting.SettingFragment;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -26,5 +34,27 @@ public class HomeActivity extends AppCompatActivity {
 
         currentUser = FirebaseAuth.getInstance().getCurrentUser();
 
+        setupNavigationBottomAppBar();
+
+
+    }
+
+    private void setupNavigationBottomAppBar() {
+
+        binding.chat.setOnClickListener(v -> setFragment(new HomeFragment()));
+
+        binding.people.setOnClickListener(v -> setFragment(new FriendFragment()));
+
+        binding.setting.setOnClickListener(v -> setFragment(new SettingFragment()));
+    }
+
+    private void setFragment(Fragment fr) {
+        FragmentManager fm = getSupportFragmentManager();
+        fm.beginTransaction().replace(R.id.section_fragment, fr).commit();
+    }
+
+    @Override
+    public boolean onNavigateUp() {
+        return super.onNavigateUp();
     }
 }
